41a59ceabe5e4886386e82064861736e3e980fc0
212007038 <rroeber@wi.rr.com>
2019-05-27 19:06:49 -0500

    First major commit of working version.

--- convert.py
+++ convert.py
@@ -13,16 +13,16 @@ import csv
 ###############################################################################
 __version__ = '1.0'  # version of script
 
-RELAY_STATE = [
+RELAY_STATE_LIST = [
     'RL', 'RA', 'LA', 'LL', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6'
 ]
 
 
-ELECTRODE_FAIL = [
+ELECTRODE_FAIL_LIST = [
     'RA', 'LA', 'LL', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'x1', 'x1', 'x1', 'x1', 'x1', 'x1', 'RL'
 ]
 
-LEAD_FAIL = [
+LEAD_FAIL_LIST = [
     'I', 'II', 'III', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'AVR', 'AVL', 'AVF'
 ]
 
@@ -33,19 +33,96 @@ MAX_ELECTRODES_LEADS = 10
 
 ###############################################################################
 # region Function region
+
+
+def convert_to_relay_header(relay_name_list):
+    # Build the header for the electrode relays
+    relay_header_string = ''
+    for electrode_relay_string in relay_name_list:
+        relay_header_string = relay_header_string + electrode_relay_string + ' Connection State, '
+
+    # Strip junk off end...
+    relay_header_string = relay_header_string[:-2]
+
+    return relay_header_string
+
+
 def convert_to_relay_state(relay_state):
     # Convert from ascii hex to number
     relay_number = int(relay_state, 16)
 
     # Convert relay state (really electrode state) to CSV string
-    for i in range(MAX_ELECTRODES_LEADS):
-        if relay_number
+    relay_state_string = ''
+    for i in range(len(RELAY_STATE_LIST)):
+        if relay_number & (1 << i):
+            relay_state_string = relay_state_string + 'OFF, '
+        else:
+            relay_state_string = relay_state_string + 'ON, '
+
+    # Strip junk off end...
+    relay_state_string = relay_state_string[:-2]
+
+    return relay_state_string
+
+
+def convert_to_electrode_header(electrode_status_list):
+    # Build the header for the electrode status
+    electrode_status_header_string = ''
+    for electrode_status_string in electrode_status_list:
+        electrode_status_header_string = electrode_status_header_string + electrode_status_string + ' Connection State, '
+
+    # Strip junk off end...
+    electrode_status_header_string = electrode_status_header_string[:-2]
+
+    return electrode_status_header_string
 
 
 def convert_to_electrode_state(electrode_state):
+    # Convert from ascii hex to number
+    electrode_number = int(electrode_state, 16)
+
+    # Convert relay state (really electrode state) to CSV string
+    electrode_state_string = ''
+    for i in range(len(ELECTRODE_FAIL_LIST)):
+        if electrode_number & (1 << i):
+            electrode_state_string = electrode_state_string + 'OFF, '
+        else:
+            electrode_state_string = electrode_state_string + 'ON, '
+
+    # Strip junk off end...
+    electrode_state_string = electrode_state_string[:-2]
+
+    return electrode_state_string
+
+
+def convert_to_lead_header(lead_list):
+    # Build the header for the electrode status
+    lead_status_header_string = ''
+    for lead_status_string in lead_list:
+        lead_status_header_string = lead_status_header_string + lead_status_string + ' Connection State, '
+
+    # Strip junk off end...
+    lead_status_header_string = lead_status_header_string[:-2]
+
+    return lead_status_header_string
+
 
 def convert_to_lead_state(lead_state):
+    # Convert from ascii hex to number
+    lead_number = int(lead_state, 16)
 
+    # Convert relay state (really electrode state) to CSV string
+    lead_state_string = ''
+    for i in range(len(LEAD_FAIL_LIST)):
+        if lead_number & (1 << i):
+            lead_state_string = lead_state_string + 'OFF, '
+        else:
+            lead_state_string = lead_state_string + 'ON, '
+
+    # Strip junk off end...
+    lead_state_string = lead_state_string[:-2]
+
+    return lead_state_string
 
 
 ###############################################################################
@@ -69,7 +146,7 @@ def main(arg_list=None):
     parser.add_argument('-i', dest='csv_input_file',
                         help='name of LabView ECG lead fail CSV result file', required=True)
     parser.add_argument('-o', dest='csv_output_file',
-                        help='name of CSV output file to write new ecg lead fail statistics to', required=False)
+                        help='name of CSV output file to write new ecg lead fail statistics to', required=True)
     parser.add_argument('-v', dest='verbose', default=False, action='store_true',
                         help='verbose output flag', required=False)
     parser.add_argument('--version', action='version', help='Print version.',
@@ -77,6 +154,11 @@ def main(arg_list=None):
 
     # Parse the command line arguments
     args = parser.parse_args(arg_list)
+
+    relay_state_header = convert_to_relay_header(RELAY_STATE_LIST)
+    electrode_state_header = convert_to_electrode_header(ELECTRODE_FAIL_LIST)
+    lead_state_header = convert_to_lead_header(LEAD_FAIL_LIST)
+    print(relay_state_header + ', ' + electrode_state_header + ', ' + lead_state_header)
 
     ###############################################################################
     # Test for existence of the LeCroy file.
@@ -89,18 +171,32 @@ def main(arg_list=None):
     # endregion
 
     # Open input CSV...
-    with open(args.csv_input_file) as csvfile:
-        read_csv = csv.reader(csvfile, delimiter=',')
+    with open(args.csv_input_file) as cvs_input_file:
+        read_csv = csv.reader(cvs_input_file, delimiter=',')
 
         # Read the header.
         headers = next(read_csv, None)
-        print('HEADERS: {}'.format(headers))
+        #print('Reference : {}'.format(headers[0]))
 
-        # Now process the rest...
-        for row in read_csv:
-            relay_state = convert_to_relay_state(row[1])
-            electrode_state = convert_to_electrode_state(row[2])
-            lead_state = convert_to_lead_state(row[3])
+        # Now read the 1st data row to get the reference electrode settings and then rewind...
+        first_data_row = next(read_csv, None)
+        reference_electrode_setting = first_data_row[0] # get the settings
+        
+
+        print(reference_electrode_setting)
+
+
+        # Okay, input file successfully opened, time to open the output file and start writing...
+        with open(args.csv_output_file, 'w') as cvs_output_file:
+            print(relay_state_header + ', ' + electrode_state_header + ', ' + lead_state_header, file=cvs_output_file)
+            # Now process the rest...
+            for row in read_csv:
+                relay_state = convert_to_relay_state(row[1])
+                electrode_state = convert_to_electrode_state(row[2])
+                lead_state = convert_to_lead_state(row[3])
+
+                # Print the big row...
+                print(relay_state + ", " + electrode_state + ', ' + lead_state, file=cvs_output_file)
 
 
 
